6e4fb68d2ba133a7639e94b8d7629280e62cab9e
LNX#.CREW <50340591+PkuLNX@users.noreply.github.com>
2019-09-25 12:29:35 +0700

    Create dash.php

---
+++ HTML/Access Login Black Room/dash.php
@@ -0,0 +1,10 @@
+<?php
+echo '
+		<body bgcolor="black">
+		<center>
+			<br><br>
+			<font face="Calibri" size="5" color="white">Welcome to L4X Dashboard!</font>
+			<br><br><br>
+			<a href="index.html" style="color: silver; font-size:14px; letter-spacing:3px; font-family:Calibri; ">EXIT</a>
+		';
+		?>
